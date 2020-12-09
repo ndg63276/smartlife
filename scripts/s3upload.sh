a25ad55a94987290f50a02b0bef8be843b63403b
Mark Williams <24956497+ndg63276@users.noreply.github.com>
2020-12-09 13:08:21 +0000

    Create an invalidation for Cloudfront when uploading to s3

--- scripts/s3upload.sh
+++ scripts/s3upload.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 aws s3 sync . s3://smartathome.co.uk/smartlife/ --exclude ".git*" --exclude "scripts/*" --exclude "images/*"
+aws cloudfront create-invalidation --distribution-id E1M3QR4XNVBQ5H --paths "/smartlife/*"
